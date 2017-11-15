4de64e76d3ce557256fb167674eafc141db0181e
NAKAJIMA Takaaki <takaaki.nakajima@mac.com>
2017-11-16 06:14:35 +0900

    add a comment

--- export.go
+++ export.go
@@ -10,6 +10,10 @@ import (
 
 // CSV ファイルへチャットログを書き出す
 func dectateCSV(msg Message) {
+	// NOTE サーバ側の timezone がそれぞれで異なると、
+	// 同時刻でも別の日付ログファイルに入る可能性あり。
+	// マージ後に考えれば良いか。
+	// キャッシュ層を入れれば、そこで吸収できるはず。
 	fname := fmt.Sprintf("%s-%s.csv", msg.Room, time.Now().Format("20060102"))
 	f, err := os.OpenFile(fname, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
